df567502f85f1490e238c47badaacc0c32152659
chiba <chiba@30ef5769-5b8d-40dd-aea6-55b5d6557bb3>
2003-12-25 13:58:53 +0000

    changed the version string.
    git-svn-id: http://anonsvn.jboss.org/repos/javassist/trunk@60 30ef5769-5b8d-40dd-aea6-55b5d6557bb3

--- src/main/javassist/CtClass.java
+++ src/main/javassist/CtClass.java
@@ -36,7 +36,7 @@ public abstract class CtClass {
     /**
      * The version number of this release.
      */
-    public static final String version = "2.7 alpha 6";
+    public static final String version = "2.7 beta 2";
 
     /**
      * Prints the version number and the copyright notice.
