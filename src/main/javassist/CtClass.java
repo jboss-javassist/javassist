5404f1e1866e3ce6132a538f5d1caffe11c0bc73
chiba <chiba@30ef5769-5b8d-40dd-aea6-55b5d6557bb3>
2006-02-22 01:17:31 +0000

    (almost) final updates for 3.1 release
    git-svn-id: http://anonsvn.jboss.org/repos/javassist/trunk@251 30ef5769-5b8d-40dd-aea6-55b5d6557bb3

--- src/main/javassist/CtClass.java
+++ src/main/javassist/CtClass.java
@@ -51,7 +51,7 @@ public abstract class CtClass {
     /**
      * The version number of this release.
      */
-    public static final String version = "3.1RC2";
+    public static final String version = "3.1";
 
     /**
      * Prints the version number and the copyright notice.
