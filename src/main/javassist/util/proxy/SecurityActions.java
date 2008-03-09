18869e66d2b785411f35e409a8b43e233ead6831
chiba <chiba@30ef5769-5b8d-40dd-aea6-55b5d6557bb3>
2008-03-09 13:07:12 +0000

    preparation for 3.7.1 release
    git-svn-id: http://anonsvn.jboss.org/repos/javassist/trunk@421 30ef5769-5b8d-40dd-aea6-55b5d6557bb3

--- src/main/javassist/util/proxy/SecurityActions.java
+++ src/main/javassist/util/proxy/SecurityActions.java
@@ -23,142 +23,113 @@ import java.security.PrivilegedAction;
 import java.security.PrivilegedActionException;
 import java.security.PrivilegedExceptionAction;
 
-class SecurityActions
-{
-    static Method[] getDeclaredMethods(final Class clazz)
-    {
-       if (System.getSecurityManager() == null)
-       {
-          return clazz.getDeclaredMethods();
-       }
-       else
-       {
-          return (Method[])AccessController.doPrivileged(new PrivilegedAction() {
-
-            public Object run()
-            {
-               return clazz.getDeclaredMethods();
-            }});
-       }
+class SecurityActions {
+    static Method[] getDeclaredMethods(final Class clazz) {
+        if (System.getSecurityManager() == null)
+            return clazz.getDeclaredMethods();
+        else {
+            return (Method[]) AccessController
+                    .doPrivileged(new PrivilegedAction() {
+                        public Object run() {
+                            return clazz.getDeclaredMethods();
+                        }
+                    });
+        }
     }
-    
-    static Constructor[] getDeclaredConstructors(final Class clazz)
-    {
-       if (System.getSecurityManager() == null)
-       {
-          return clazz.getDeclaredConstructors();
-       }
-       else
-       {
-          return (Constructor[])AccessController.doPrivileged(new PrivilegedAction() {
 
-            public Object run()
-            {
-               return clazz.getDeclaredConstructors();
-            }});
-       }
+    static Constructor[] getDeclaredConstructors(final Class clazz) {
+        if (System.getSecurityManager() == null)
+            return clazz.getDeclaredConstructors();
+        else {
+            return (Constructor[]) AccessController
+                    .doPrivileged(new PrivilegedAction() {
+                        public Object run() {
+                            return clazz.getDeclaredConstructors();
+                        }
+                    });
+        }
     }
-    
-    static Method getDeclaredMethod(final Class clazz, final String name, final Class[] types) throws NoSuchMethodException
-    {
-       if (System.getSecurityManager() == null)
-       {
-          return clazz.getDeclaredMethod(name, types);
-       }
-       else
-       {
-          try
-          {
-             return (Method)AccessController.doPrivileged(new PrivilegedExceptionAction() {
 
-                public Object run()throws Exception
-                {
-                   return clazz.getDeclaredMethod(name, types);
-                }});
-          }
-          catch(PrivilegedActionException e)
-          {
-             if (e.getCause() instanceof NoSuchMethodException)
-             {
-                throw (NoSuchMethodException)e.getCause();
-             }
-             throw new RuntimeException(e.getCause());
-          }
-       }
-    }
-    
-    static Constructor getDeclaredConstructor(final Class clazz, final Class[] types) throws NoSuchMethodException
-    {
-       if (System.getSecurityManager() == null)
-       {
-          return clazz.getDeclaredConstructor(types);
-       }
-       else
-       {
-          try
-         {
-            return (Constructor)AccessController.doPrivileged(new PrivilegedExceptionAction() {
-
-               public Object run() throws Exception
-               {
-                  return clazz.getDeclaredConstructor(types);
-               }});
-         }
-         catch (PrivilegedActionException e)
-         {
-            if (e.getCause() instanceof NoSuchMethodException)
-            {
-               throw (NoSuchMethodException)e.getCause();
+    static Method getDeclaredMethod(final Class clazz, final String name,
+            final Class[] types) throws NoSuchMethodException {
+        if (System.getSecurityManager() == null)
+            return clazz.getDeclaredMethod(name, types);
+        else {
+            try {
+                return (Method) AccessController
+                        .doPrivileged(new PrivilegedExceptionAction() {
+                            public Object run() throws Exception {
+                                return clazz.getDeclaredMethod(name, types);
+                            }
+                        });
             }
-            throw new RuntimeException(e.getCause());
-         }
-       }
-    }    
-    
-    static void setAccessible(final AccessibleObject ao, final boolean accessible)
-    {
-       if (System.getSecurityManager() == null)
-       {
-          ao.setAccessible(accessible);
-       }
-       else
-       {
-          AccessController.doPrivileged(new PrivilegedAction() {
+            catch (PrivilegedActionException e) {
+                if (e.getCause() instanceof NoSuchMethodException)
+                    throw (NoSuchMethodException) e.getCause();
 
-            public Object run() 
-            {
-               ao.setAccessible(accessible);
-               return null;
-            }});
-       }
-    }    
-    
-    static void set(final Field fld, final Object target, final Object value) throws IllegalAccessException
-    {
-       if (System.getSecurityManager() == null)
-       {
-          fld.set(target, value);
-       }
-       else
-       {
-          try
-         {
-            AccessController.doPrivileged(new PrivilegedExceptionAction() {
-
-               public Object run() throws Exception 
-               {
-                  fld.set(target, value);
-                  return null;
-               }});
-         }
-         catch (PrivilegedActionException e)
-         {
-            if (e.getCause() instanceof NoSuchMethodException)
-            {
-               throw (IllegalAccessException)e.getCause();
+                throw new RuntimeException(e.getCause());
             }
-            throw new RuntimeException(e.getCause());
-         }
-       }
-    }    
+        }
+    }
+
+    static Constructor getDeclaredConstructor(final Class clazz,
+                                              final Class[] types)
+        throws NoSuchMethodException
+    {
+        if (System.getSecurityManager() == null)
+            return clazz.getDeclaredConstructor(types);
+        else {
+            try {
+                return (Constructor) AccessController
+                        .doPrivileged(new PrivilegedExceptionAction() {
+                            public Object run() throws Exception {
+                                return clazz.getDeclaredConstructor(types);
+                            }
+                        });
+            }
+            catch (PrivilegedActionException e) {
+                if (e.getCause() instanceof NoSuchMethodException)
+                    throw (NoSuchMethodException) e.getCause();
+
+                throw new RuntimeException(e.getCause());
+            }
+        }
+    }
+
+    static void setAccessible(final AccessibleObject ao,
+                              final boolean accessible) {
+        if (System.getSecurityManager() == null)
+            ao.setAccessible(accessible);
+        else {
+            AccessController.doPrivileged(new PrivilegedAction() {
+                public Object run() {
+                    ao.setAccessible(accessible);
+                    return null;
+                }
+            });
+        }
+    }
+
+    static void set(final Field fld, final Object target, final Object value)
+        throws IllegalAccessException
+    {
+        if (System.getSecurityManager() == null)
+            fld.set(target, value);
+        else {
+            try {
+                AccessController.doPrivileged(new PrivilegedExceptionAction() {
+                    public Object run() throws Exception {
+                        fld.set(target, value);
+                        return null;
+                    }
+                });
+            }
+            catch (PrivilegedActionException e) {
+                if (e.getCause() instanceof NoSuchMethodException)
+                    throw (IllegalAccessException) e.getCause();
+
+                throw new RuntimeException(e.getCause());
+            }
+        }
+    }
 }
