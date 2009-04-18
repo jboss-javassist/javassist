29bf701b71069139ccf59868ebcb7352b9f8049f
chiba <chiba@30ef5769-5b8d-40dd-aea6-55b5d6557bb3>
2009-04-18 15:08:19 +0000

    for fixing JASSIST-81
    git-svn-id: http://anonsvn.jboss.org/repos/javassist/trunk@478 30ef5769-5b8d-40dd-aea6-55b5d6557bb3

--- src/main/javassist/bytecode/ClassFile.java
+++ src/main/javassist/bytecode/ClassFile.java
@@ -47,9 +47,27 @@ public final class ClassFile {
 
     /**
      * The major version number of class files
+     * for JDK 1.1.
+     */
+    public static final int JAVA_1 = 45;
+
+    /**
+     * The major version number of class files
+     * for JDK 1.2.
+     */
+    public static final int JAVA_2 = 46;
+
+    /**
+     * The major version number of class files
      * for JDK 1.3.
      */
     public static final int JAVA_3 = 47;
+
+    /**
+     * The major version number of class files
+     * for JDK 1.4.
+     */
+    public static final int JAVA_4 = 48;
 
     /**
      * The major version number of class files
