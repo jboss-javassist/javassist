121a67e8df095ee2159ed987b0c64728fe3a39ed
chiba <chiba@30ef5769-5b8d-40dd-aea6-55b5d6557bb3>
2011-07-08 08:37:50 +0000

    added the Apache license.
    git-svn-id: http://anonsvn.jboss.org/repos/javassist/trunk@584 30ef5769-5b8d-40dd-aea6-55b5d6557bb3

--- src/main/javassist/CtClass.java
+++ src/main/javassist/CtClass.java
@@ -1,11 +1,12 @@
 /*
  * Javassist, a Java-bytecode translator toolkit.
- * Copyright (C) 1999-2007 Shigeru Chiba. All Rights Reserved.
+ * Copyright (C) 1999- Shigeru Chiba. All Rights Reserved.
  *
  * The contents of this file are subject to the Mozilla Public License Version
  * 1.1 (the "License"); you may not use this file except in compliance with
  * the License.  Alternatively, the contents of this file may be used under
- * the terms of the GNU Lesser General Public License Version 2.1 or later.
+ * the terms of the GNU Lesser General Public License Version 2.1 or later,
+ * or the Apache License Version 2.0.
  *
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
@@ -52,7 +53,7 @@ public abstract class CtClass {
     /**
      * The version number of this release.
      */
-    public static final String version = "3.14.0.GA";
+    public static final String version = "3.15.0.GA";
 
     /**
      * Prints the version number and the copyright notice.
@@ -63,7 +64,7 @@ public abstract class CtClass {
      */
     public static void main(String[] args) {
         System.out.println("Javassist version " + CtClass.version);
-        System.out.println("Copyright (C) 1999-2010 Shigeru Chiba."
+        System.out.println("Copyright (C) 1999-2011 Shigeru Chiba."
                            + " All Rights Reserved.");
     }
 
--- src/main/javassist/bytecode/annotation/LongMemberValue.java
+++ src/main/javassist/bytecode/annotation/LongMemberValue.java
@@ -5,7 +5,8 @@
  * The contents of this file are subject to the Mozilla Public License Version
  * 1.1 (the "License"); you may not use this file except in compliance with
  * the License.  Alternatively, the contents of this file may be used under
- * the terms of the GNU Lesser General Public License Version 2.1 or later.
+ * the terms of the GNU Lesser General Public License Version 2.1 or later,
+ * or the Apache License Version 2.0.
  *
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
