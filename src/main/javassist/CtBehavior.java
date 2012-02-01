075e0b82876cc274dedf125a44b5288d6e594b3f
chiba <chiba@30ef5769-5b8d-40dd-aea6-55b5d6557bb3>
2012-02-01 09:56:21 +0000

    fixed JASSIST-149
    git-svn-id: http://anonsvn.jboss.org/repos/javassist/trunk@607 30ef5769-5b8d-40dd-aea6-55b5d6557bb3

--- src/main/javassist/CtBehavior.java
+++ src/main/javassist/CtBehavior.java
@@ -651,7 +651,15 @@ public abstract class CtBehavior extends CtMember {
     /**
      * Modifies the method/constructor body.
      *
+     * <p>While executing this method, only <code>replace()</code>
+     * in <code>Expr</code> is available for bytecode modification.
+     * Other methods such as <code>insertBefore()</code> may collapse
+     * the bytecode because the <code>ExprEditor</code> loses
+     * its current position.  
+     *
      * @param editor            specifies how to modify.
+     * @see javassist.expr.Expr#replace(String)
+     * @see #insertBefore(String)
      */
     public void instrument(ExprEditor editor)
         throws CannotCompileException
