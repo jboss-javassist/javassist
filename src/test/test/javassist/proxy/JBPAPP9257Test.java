991747ba2c2a9472daaa3e51582aeb8ccf4ac791
chiba <chiba@30ef5769-5b8d-40dd-aea6-55b5d6557bb3>
2012-08-03 20:24:33 +0000

    JASSIST-127 and JBPAPP9257
    git-svn-id: http://anonsvn.jboss.org/repos/javassist/trunk@644 30ef5769-5b8d-40dd-aea6-55b5d6557bb3

--- src/test/test/javassist/proxy/JBPAPP9257Test.java
+++ src/test/test/javassist/proxy/JBPAPP9257Test.java
@@ -29,7 +29,7 @@ public class JBPAPP9257Test extends TestCase {
         };
         Foo foo = (Foo) c.newInstance();
         try {
-            ((Proxy)foo).setHandler(mi);
+            ((ProxyObject)foo).setHandler(mi);
             fail("foo is a ProxyObject!");
         } catch (ClassCastException e) {}
         ((Proxy)foo).setHandler(mi);
