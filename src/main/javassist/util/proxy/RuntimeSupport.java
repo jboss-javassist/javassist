962a750f485d1a4c9bd431902f4bdf63b35bf3c4
chiba <chiba@30ef5769-5b8d-40dd-aea6-55b5d6557bb3>
2012-08-03 21:01:48 +0000

    fixed JASSIST-163
    git-svn-id: http://anonsvn.jboss.org/repos/javassist/trunk@645 30ef5769-5b8d-40dd-aea6-55b5d6557bb3

--- src/main/javassist/util/proxy/RuntimeSupport.java
+++ src/main/javassist/util/proxy/RuntimeSupport.java
@@ -50,7 +50,10 @@ public class RuntimeSupport {
                                     String thisMethod, int index,
                                     String desc, java.lang.reflect.Method[] methods)
     {
-        if (methods[index] == null) {
+        /* Once methods[index] and methods[index + 1] are set to non-null, 
+         * then their values never change.
+         */
+        if (methods[index] == null || methods[index + 1] == null) {
             Method m1 = thisMethod == null ? null
                                            : findMethod(self, thisMethod, desc);
             Method m0 = findSuperMethod(self, superMethod, desc);
