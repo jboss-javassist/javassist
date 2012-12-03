720eab860612a0bf5497968303bdfc5283a3143a
chiba <chiba@30ef5769-5b8d-40dd-aea6-55b5d6557bb3>
2012-12-03 10:40:00 +0000

    for 3.17.1 release
    git-svn-id: http://anonsvn.jboss.org/repos/javassist/trunk@694 30ef5769-5b8d-40dd-aea6-55b5d6557bb3

--- src/main/javassist/CtClass.java
+++ src/main/javassist/CtClass.java
@@ -69,7 +69,7 @@ public abstract class CtClass {
     /**
      * The version number of this release.
      */
-    public static final String version = "3.17.1-snapshot";
+    public static final String version = "3.17.1-GA";
 
     /**
      * Prints the version number and the copyright notice.
