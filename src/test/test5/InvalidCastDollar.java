4cf21faf5416cb73988ba7556fdb91f6f604b311
chibash <chiba@javassist.org>
2015-12-08 00:10:58 +0900

    fixes JASSIST-255 Primitive array return values cause invalid cast to Object

---
+++ src/test/test5/InvalidCastDollar.java
@@ -0,0 +1,11 @@
+package test5;
+
+public class InvalidCastDollar {
+    public static byte[] arrayReturn() {
+        return new byte[12];
+    }
+
+    public static int intReturn() {
+        return 23;
+    }
+}
