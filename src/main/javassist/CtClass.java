bcfdc3436616604645971899249e9a1abbd5434f
chibash <chiba@javassist.org>
2018-11-01 01:05:31 +0900

    updates for release 3.24.0-GA

--- src/main/javassist/CtClass.java
+++ src/main/javassist/CtClass.java
@@ -69,7 +69,7 @@ public abstract class CtClass {
     /**
      * The version number of this release.
      */
-    public static final String version = "3.24.0-RC";
+    public static final String version = "3.24.0-GA";
 
     /**
      * Prints the version number and the copyright notice.
