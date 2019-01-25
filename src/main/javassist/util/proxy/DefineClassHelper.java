1b84304638790522bed13bda3d75a3e904682915
michalkurka <michalk@h2o.ai>
2019-01-25 11:30:06 -0800

    Proposed fixes in DefineClassHelper & DefinePackageHelper
    Don't try to clean-up after making a protected-final method accessible.
    No synchronization with concurrent access can cause errors.

--- src/main/javassist/util/proxy/DefineClassHelper.java
+++ src/main/javassist/util/proxy/DefineClassHelper.java
@@ -219,9 +219,6 @@ public class DefineClassHelper {
                 if (e instanceof RuntimeException) throw (RuntimeException) e;
                 throw new CannotCompileException(e);
             }
-            finally {
-                SecurityActions.setAccessible(defineClass, false);
-            }
         }
     }
 
