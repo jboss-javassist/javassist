b15f6c06408ed28d7f49725a962fda510b092951
Scott Marlow <smarlow@redhat.com>
2019-04-18 09:35:00 -0400

    updates for release 3.23.2

--- src/main/javassist/CtClass.java
+++ src/main/javassist/CtClass.java
@@ -69,7 +69,7 @@ public abstract class CtClass {
     /**
      * The version number of this release.
      */
-    public static final String version = "3.23.1-GA";
+    public static final String version = "3.23.2-GA";
 
     /**
      * Prints the version number and the copyright notice.
